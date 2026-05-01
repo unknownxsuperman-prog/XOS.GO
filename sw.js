16b5056fcdf72351b4599eca219cddfbb4890e91
unknownxsuperman-prog <unknownxsuperman@gmail.com>
2026-05-01 19:45:49 +0530

    Refactor service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-const CACHE_NAME = 'xos-go-v1';
+const CACHE_NAME = "xosgo-v1";
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/xosgo.png'
+  "./",
+  "./index.html",
+  "./logo.png",
+  "./wallpaper.jpg",
+  "./users.js",
+  "./places.js",
+  "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+// Install
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+// Activate
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => caches.match("./index.html"));
     })
   );
 });
