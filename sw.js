bca2d6fa8e21b7ac63285318d05d94ec30de6d4a
unknownxsuperman-prog <unknownxsuperman@gmail.com>
2026-05-01 20:13:51 +0530

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,43 +1,35 @@
-const CACHE_NAME = "xosgo-v1";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./xosgo.png",
-  "./wallpaper.jpg",
-  "./users.js",
-  "./places.js",
-  "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+const CACHE_NAME = 'xosgo-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './users.js',
+  './places.js',
+  './logo.png',
+  './wallpaper.jpg',
+  'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
 ];
 
-// Install
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// Activate
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
-      )
-    )
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
-  self.clients.claim();
 });
 
-// Fetch
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request).catch(() => caches.match("./index.html"));
-    })
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
   );
 });
