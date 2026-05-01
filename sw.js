e26ed8fc779b6d0df644d0bdba0d1a638db6bb32
unknownxsuperman-prog <unknownxsuperman@gmail.com>
2026-05-01 20:59:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,21 @@
-const CACHE_NAME = 'xosgo-v1';
-const urlsToCache = [
+const CACHE_NAME = 'xos-go-v1';
+const ASSETS = [
   './',
   './index.html',
-  './users.js',
-  './places.js',
   './logo.png',
   './wallpaper.jpg',
-  'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+  './users.js',
+  './places.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
-    ))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
